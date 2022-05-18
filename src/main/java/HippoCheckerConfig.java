a08051430e5bb6b739d5c8155052d7dc06d0cf0a
Marnix Kok <marnix@xinsolutions.co.nz>
2022-05-18 21:58:49 +1200

    Changes:
    * created simple wrapper for HippoChecker utility that reads information from environment variables, generates required configuration files and starts running the check

---
+++ src/main/java/HippoCheckerConfig.java
@@ -0,0 +1,49 @@
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Author: Marnix Kok
+ *
+ * Purpose:
+ *
+ *      To capture configuration information about how to run the checker.
+ *
+ */
+public class HippoCheckerConfig {
+
+    public int port = 3306;
+    public String host;
+    public String database;
+    public String username;
+    public String password;
+
+    /**
+     * @return a hippo checker config object initialised from environment variables
+     */
+    public static HippoCheckerConfig fromEnvironmentVariables() {
+
+        List<String> envVars = Arrays.asList("MYSQL_HOST", "MYSQL_DATABASE", "MYSQL_USERNAME", "MYSQL_PASSWORD");
+        for (String envName : envVars) {
+            if (!System.getenv().containsKey(envName)) {
+                throw new IllegalStateException("The configuration cannot be created, " + envName + " is not an environment variable");
+            }
+        }
+
+        HippoCheckerConfig config = new HippoCheckerConfig();
+        config.host = System.getenv("MYSQL_HOST");
+        config.database = System.getenv("MYSQL_DATABASE");
+        config.username = System.getenv("MYSQL_USERNAME");
+        config.password = System.getenv("MYSQL_PASSWORD");
+
+        if (System.getenv().containsKey("MYSQL_PORT")) {
+            if (NumberUtils.isDigits(System.getenv("MYSQL_PORT"))) {
+                config.port = Integer.parseInt(System.getenv("MYSQL_PORT"));
+            }
+        }
+
+        return config;
+    }
+}
+
---
+++ src/main/java/LambdaHippoChecker.java
@@ -0,0 +1,164 @@
+import org.apache.jackrabbit.core.CheckerRepository;
+import org.apache.jackrabbit.core.config.RepositoryConfig;
+import org.apache.tika.io.IOUtils;
+import org.onehippo.cms7.repository.checker.CheckerOptions;
+import org.onehippo.cms7.repository.checker.Main;
+import org.xml.sax.InputSource;
+
+import javax.jcr.RepositoryException;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+/**
+ * Author: Marnix Kok
+ *
+ * Purpose:
+ *
+ *      Run a check on a repository
+ *
+ */
+public class LambdaHippoChecker {
+
+    /**
+     * Logger
+     */
+    private static final Logger LOG = Logger.getLogger("hippo-checker");
+
+    /**
+     * Stores the configuration
+     */
+    private HippoCheckerConfig config;
+
+    /**
+     * Initialise data-members
+     *
+     * @param config the configuration for the chcker
+     */
+    public LambdaHippoChecker(HippoCheckerConfig config) {
+        this.config = config;
+    }
+
+    /**
+     * Initialise the configuration files, especially the repository.xml by replacing
+     * the placeholders with information from the configuration we have received from
+     * the user.
+     *
+     * @throws IOException
+     */
+    public void initialise() throws IOException {
+        // read dummy repository.xml
+        InputStream stream = this.getClass().getResourceAsStream("/repository.xml");
+        StringWriter strWriter = new StringWriter();
+        IOUtils.copy(stream, strWriter);
+
+        String repoXml = strWriter.toString();
+        String finalRepoXml = (
+            repoXml
+                .replace("${host}", config.host)
+                .replace("${port}", String.format("%d", config.port))
+                .replace("${database}", config.database)
+                .replace("${username}", config.username)
+                .replace("${password}", config.password)
+        );
+
+        FileWriter fWriter = new FileWriter("/tmp/repository.xml");
+        IOUtils.write(finalRepoXml, fWriter);
+        fWriter.close();
+
+        LOG.info("Initialisation complete.");
+    }
+
+    /**
+     * Clean up version history.
+     *
+     * @throws IOException
+     * @throws RepositoryException
+     * @throws URISyntaxException
+     */
+    public void cleanupVersionHistory() throws IOException, RepositoryException, URISyntaxException {
+        LOG.info("START: Starting to clean up version history in repository.");
+
+        Properties properties = new Properties();
+        properties.load(this.getClass().getResourceAsStream("/checker.properties"));
+        CheckerOptions options = new CheckerOptions(new String[] { "CLEANVH" }, properties);
+
+        CheckerRepository repository = null;
+        try {
+            repository = createRepository(options, properties);
+            repository.cleanHistory(options.cleanNonEmptyHistory());
+        }
+        finally {
+            if (repository != null) {
+                repository.shutdown();
+            }
+        }
+
+        LOG.info("DONE: Finished cleaning up the version history");
+    }
+
+    /**
+     * Clean up the data storage
+     *
+     * @throws IOException
+     * @throws RepositoryException
+     * @throws URISyntaxException
+     */
+    public void cleanupDataStorage()  throws IOException, RepositoryException, URISyntaxException {
+
+        LOG.info("START: Starting to clean up data storage in repository.");
+
+        Properties properties = new Properties();
+        properties.load(this.getClass().getResourceAsStream("/checker.properties"));
+        CheckerOptions options = new CheckerOptions(new String[] { "CLEANDS" }, properties);
+
+        CheckerRepository repository = null;
+        try {
+            repository = createRepository(options, properties);
+            repository.cleanBinaries();
+        }
+        finally {
+            if (repository != null) {
+                repository.shutdown();
+            }
+        }
+
+        LOG.info("DONE: Datastorage cleanup completed.");
+    }
+
+
+    /**
+     * Create a checker repository instance based on the repository.xml written to /tmp by `initialise()`.
+     *
+     * @param options
+     * @param properties
+     * @return
+     *
+     * @throws RepositoryException
+     * @throws URISyntaxException
+     */
+    protected CheckerRepository createRepository(CheckerOptions options, Properties properties) throws RepositoryException, URISyntaxException {
+        InputSource inputSource = new InputSource("file:///tmp/repository.xml");
+        return new CheckerRepository(RepositoryConfig.create(inputSource, properties), options);
+    }
+
+    /**
+     * Run
+     * @param args
+     */
+    public static void main(String[] args) {
+        HippoCheckerConfig config = HippoCheckerConfig.fromEnvironmentVariables();
+        LambdaHippoChecker checker = new LambdaHippoChecker(config);
+        try {
+            checker.initialise();
+            checker.cleanupVersionHistory();
+            checker.cleanupDataStorage();
+        }
+        catch (Exception ex) {
+            LOG.warning("Could not execute hippo checker, caused by: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+}
